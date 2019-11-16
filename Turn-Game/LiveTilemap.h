11d69ba7b20571e1a4cfdee49a481e92d50b910b
AndrewCRichter <57776288+AndrewCRichter@users.noreply.github.com>
2019-11-16 14:37:51 -0500

    Trying to make Git behave

---
+++ Turn-Game/LiveTilemap.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class LiveTilemap : public sf::Drawable, public sf::Transformable
+{
+public:
+	
+	LiveTilemap(const sf::Texture* tileset, int tileWidth, int tileHeight,int tilesPerRow);
+	bool update(std::vector<std::vector<int>> tiles);
+	~LiveTilemap();
+
+private:
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	const sf::Texture* tileset;
+	sf::VertexArray verts;
+	int t_width, t_height, perRow;
+
+}
