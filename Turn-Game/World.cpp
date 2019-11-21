edcd9153a29954fc38fbcdad3e01ee6fa00581e7
Evan Fisher <34456732+EvanRFisher@users.noreply.github.com>
2019-11-21 13:34:11 -0500

    Clarified code and corrected some errors in World.cpp. Added texture files to project.

--- Turn-Game/World.cpp
+++ Turn-Game/World.cpp
@@ -2,21 +2,35 @@
 #include <fstream>
 #include <cmath>
 #include <cstdlib>
+
+int min(int a, int b) { return a < b ? a : b; }
+
 std::vector<int> WorldGenHelper(std::vector<std::vector<int>> world, int height, int xStart, int xEnd, int zStart, int zEnd)
 {
 	if (xEnd + 1 > xStart)
 	{
-		world[xStart + (xEnd - xStart) / 2][zStart] = std::cmath.rand() % (2 * (std::abs(world[xStart][zStart] - world[xEnd][zStart]) + std::cmath.min(world[xStart][zStart] ,world[xEnd][zStart]));
-		world[xStart + (xEnd - xStart) / 2][zEnd] = std::cmath.rand() % (2 * (std::abs(world[xStart][zEnd] - world[xEnd][zStart]) + std::cmath.min(world[xStart][zEnd] ,world[xEnd][zEnd]));
+		int diff1 = std::abs(world[xStart][zStart] - world[xEnd][zStart]),
+			diff2 = std::abs(world[xStart][zEnd]   - world[xEnd][zEnd]); /*Assuming that this should this be zEnd for the second one istead of zStart*/
+		int min1 = min(world[xStart][zStart], world[xEnd][zStart]),
+			min2 = min(world[xStart][zEnd],   world[xEnd][zEnd]);
+		world[xStart + (xEnd - xStart) / 2][zStart] = std::rand() % (2 * diff1) + min1;
+		world[xStart + (xEnd - xStart) / 2][zEnd]   = std::rand() % (2 * diff2) + min2;
 	}
 	if (zEnd + 1 > zStart)
 	{
-		world[xStart][zStart + (zEnd - zStart) / 2] = std::cmath.rand() % (2 * (std::abs(world[xStart][zStart] - world[xStart][zEnd]) + std::cmath.min(world[xStart][zStart] ,world[xStart][zEnd]));
-		world[xEnd][zStart + (zEnd - zStart) / 2] = std::cmath.rand() % (2 * (std::abs(world[xEnd][zStart] - world[xEnd][zEnd]) + std::cmath.min(world[xEnd][zStart] - world[xEnd][zEnd]));
+		int diff1 = std::abs(world[xStart][zStart] - world[xStart][zEnd]),
+			diff2 = std::abs(world[xEnd][zStart]   - world[xEnd][zEnd]);
+		int min1 = min(world[xStart][zStart], world[xStart][zEnd]),
+			min2 = min(world[xEnd][zStart],   world[xEnd][zEnd]);
+		world[xStart + (xEnd - xStart) / 2][zStart] = std::rand() % (2 * diff1) + min1;
+		world[xStart + (xEnd - xStart) / 2][zEnd] = std::rand() % (2 * diff2) + min2;
 	}
 	if (xEnd + 1 > xStart && zEnd + 1 > zStart)
 	{
-		world[xStart + (xEnd - xStart) / 2][zStart + (zEnd - zStart) / 2] = (world[xStart][zStart] + world[xEnd][zEnd] + world[xStart][zEnd] + world[xEnd][zStart]) / 4;
+		world[xStart + (xEnd - xStart) / 2][zStart + (zEnd - zStart) / 2] = (world[xStart][zStart]
+																		   + world[xEnd][zEnd]
+																		   + world[xStart][zEnd]
+																		   + world[xEnd][zStart]) / 4;
 	}
 }
 
@@ -27,7 +41,7 @@ World::World(std::string fileName, int height, int width, int depth)
 	inputWorld.open(fileName);
 	int i, j, k;
 	inputWorld >> i >> j >> k;
-	if (inputWorld.good) {
+	if (inputWorld.good()) {
 		world.resize(depth);
 		for (i = 0; i < depth && good; ++i)
 		{
@@ -39,7 +53,7 @@ World::World(std::string fileName, int height, int width, int depth)
 				{
 					int next;
 					inputWorld >> next;
-					if (!inputWorld.good)
+					if (!inputWorld.good())
 					{
 						good = false;
 					}
@@ -71,8 +85,9 @@ std::vector<std::vector<int>> World::getSlice(int x, int z, int xDirection, int 
 }
 void WorldGen(std::string fileName, int height, int width, int depth)
 {
-	std::vector<vector<int>> flatWorld;
+	std::vector<std::vector<int>> flatWorld;
 	flatWorld.resize(depth);
+	int i;
 	for (i = 0; i < depth; ++i)
 	{
 		flatWorld[i].resize(width);
