92f9ddfaad26a477e06d5ccbeb0211a66f8c715d
AndrewCRichter <57776288+AndrewCRichter@users.noreply.github.com>
2019-11-25 15:57:25 -0500

    Updated world generation methods.

--- Turn-Game/World.cpp
+++ Turn-Game/World.cpp
@@ -3,24 +3,31 @@
 #include <cmath>
 #include <cstdlib>
 
-int min(int a, int b) { return a < b ? a : b; }
 int WorldGenVal(int x, int y)
 {
-	return std::cmath.rand() % (1.25 * (std::abs(x - y)) + std::cmath.min(x, y);
+	return std::rand() % (1.5 * (x < y ? y - x : x - y)) + x < y ? x : y;
 }
-void WorldGenHelper(std::vector<std::vector<int>> &world, int height, int xStart, int xEnd, int zStart, int zEnd)
+void WorldGenHelper(std::vector<std::vector<int>> &world, int xStart, int xEnd, int zStart, int zEnd)
 {
 	bool again = false;
 	int xMid = xStart + (xEnd - xStart) / 2,
-		zMid = zStart + (zEnd - zStart) / 2
+		zMid = zStart + (zEnd - zStart) / 2;
 	if (xEnd + 1 > xStart)
 	{
-		world[xMid][zStart] = WorldGenVal(world[xStart][zStart], world[xEnd][zStart]);
+		world[xMid][zEnd] = WorldGenVal(world[xStart][zEnd], world[xEnd][zEnd]);
+		if (zStart == 0)
+		{
+			world[xMid][zStart] = WorldGenVal(world[xStart][zStart], world[xEnd][zStart]);
+		}
 		again = true;
 	}
 	if (zEnd + 1 > zStart)
 	{
-		world[xStart][zMid] = WorldGenVal(world[xStart][zStart], world[xStart][zEnd]);
+		world[xEnd][zMid] = WorldGenVal(world[xEnd][zStart], world[xEnd][zEnd]);
+		if (xStart == 0)
+		{
+			world[xStart][zMid] = WorldGenVal(world[xStart][zStart], world[xStart][zEnd]);
+		}
 		again = true;
 	}
 	if (xEnd + 1 > xStart && zEnd + 1 > zStart)
@@ -30,10 +37,11 @@ void WorldGenHelper(std::vector<std::vector<int>> &world, int height, int xStart
 	}
 	if (again)
 	{
-		WorldGenHelper(*world, height, xStart, xMid, zStart, zMid);
-		WorldGenHelper(*world, height, xStart, xMid, zStart, zMid);
-		WorldGenHelper(*world, height, xStart, xMid, zStart, zMid);
-		WorldGenHelper(*world, height, xStart, xMid, zStart, zMid);
+		//The order matters of these lines. This order is correct.
+		WorldGenHelper(world, xStart, xMid, zStart, zMid);
+		WorldGenHelper(world, xMid, xEnd, zStart, zMid);
+		WorldGenHelper(world, xStart, xMid, zMid, zEnd);
+		WorldGenHelper(world, xMid, xEnd, zMid, zEnd);
 	}
 }
 
@@ -89,11 +97,43 @@ std::vector<std::vector<int>> World::getSlice(int x, int z, int xDirection, int 
 }
 void WorldGen(std::string fileName, int height, int width, int depth)
 {
+	std::vector<std::vector<std::vector<int>>> world;
 	std::vector<std::vector<int>> flatWorld;
+	int i, j, k;
+
 	flatWorld.resize(depth);
-	int i;
 	for (i = 0; i < depth; ++i)
 	{
 		flatWorld[i].resize(width);
+	}
+	flatWorld[0][0] = height / 2;
+	flatWorld[0][width] = height / 3;
+	flatWorld[depth][0] = height / 4;
+	flatWorld[depth][width] = height / 5;
+	WorldGenHelper(flatWorld, 0, width, 0, depth);
+
+	world.resize(depth);
+	for (i = 0; i < depth; ++i)
+	{
+		world[i].resize(width);
+		for (j = 0; j < width; ++j)
+		{
+			world.resize(height);
+			for (k = 0; k < height; ++k)
+			{
+				if (k < flatWorld[i][j])
+				{
+					world[i][j][k] = 1;
+				}
+				else if(k > flatWorld[i][j])
+				{
+					world[i][j][k] = 0;
+				}
+				else
+				{
+					world[i][j][k] = 2;
+				}
+			}
+		}
 	}
 }
